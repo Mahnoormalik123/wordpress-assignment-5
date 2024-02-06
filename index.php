58a5c9ea5530a827ee467347b9e2993e737b5e9b
Mahnoormalik123 <154795582+Mahnoormalik123@users.noreply.github.com>
2024-02-06 15:52:49 +0500

    index.php

--- index.php
+++ index.php
@@ -2,13 +2,12 @@
 // Function to print a table based on the given parameter
 function printTable($multiplier) {
     $i = 1; // Initialize the counter
-    // Use a while loop to print the table
+    // Using a while loop to print the table
     while ($i <= 10) {
         $result = $multiplier * $i;
         echo "$multiplier x $i = $result<br>";
-        $i++; // Increment the counter
+        $i++; // Increment
     }
 }
-// Call the function to print the table of 5 
 printTable(18);
 ?>
